2d12325372e057b232d2ecc5c2c811433f8e609b
rachid <rachidboudaoudi95@gmail.com>
2021-09-07 22:32:13 +0100

    Add basic layout and styling.

--- webpack.config.js
+++ webpack.config.js
@@ -4,6 +4,7 @@ const HtmlWebpackPlugin = require('html-webpack-plugin');
 module.exports = {
   entry: './src/main-src.js',
   devtool: 'inline-source-map',
+  mode: 'development',
   devServer: {
     static: './dist',
   },
