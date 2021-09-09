362ef988cdc35ecb2be5a1658c0eda43d890cbb7
rachid <rachidboudaoudi95@gmail.com>
2021-09-09 14:51:37 +0100

    Add displayData function

--- src/api.js
+++
@@ -1,9 +0,0 @@
-const postData = async (user, score) => {
-  await fetch('https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/VSMpU4FtgeRTlXkLPFBz/scores', {
-    method: 'POST',
-    headers: { 'Content-Type': 'application/json' },
-    body: JSON.stringify({ user, score }),
-  });
-};
-
-export { postData as default };
--- src/main-src.js
+++ src/main-src.js
@@ -1,2 +1,37 @@
 import './style-src.css';
-import postData from './api.js';
+
+const url = 'https://us-central1-js-capstone-backend.cloudfunctions.net/api/games/VSMpU4FtgeRTlXkLPFBz';
+const submitBtn = document.getElementById('submit');
+const refreshBtn = document.getElementById('refresh');
+
+const postData = async (user, score) => {
+  await fetch(`${url}/scores`, {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({ user, score }),
+  });
+};
+
+const addScoresToDom = (scores) => {
+  const msg = document.getElementById('msg-empty');
+  msg.parentElement.removeChild(msg);
+  scores.forEach(score => {
+    const li = document.createElement('li');
+    const userText = document.createElement('p');
+    userText.classList.add('user flex-list-item');
+    userText.innerText = score.user;
+    const scoreText = document.createElement('p');
+    scoreText.classList.add('score flex-list-item');
+    scoreText.innerText = score.score;
+    li.appendChild(userText);
+    li.appendChild(scoreText);
+  });
+};
+
+const displayData = async () => {
+  const result = await fetch(`${url}/scores`);
+  const data = await result.json();
+  const scores = data.result;
+  addScoresToDom(scores);
+};
+
